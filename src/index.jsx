dbbc423f634d15d10c60a6722778b32ac165afdb
Matheus Araujo Morais <matheus.araujo.morais@outlook.com>
2024-01-11 17:06:41 -0300

    feat: exercise 3 - useState and fetch

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Ex1 from './ex1.jsx'
 import Ex2 from './ex2.jsx'
+import Ex3 from './ex3.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <p><b>Exercício 1:</b></p> 
     <Ex1 /> <br />
     <p><b>Exercício 2:</b></p>
-    <Ex2 />
+    <Ex2 /> <br />
+    <p><b>Exercício 3:</b></p>
+    <Ex3 /> <br />
   </React.StrictMode>,
 )
